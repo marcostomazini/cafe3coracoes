9e283c398c3bafef4a1c62f41071c60b011c73ee
Marcos Tomazini <marcos.tomazini@gmail.com>
2016-01-27 17:55:05 -0200

    date solved

--- index.js
+++ index.js
@@ -20,11 +20,10 @@ var express = require('express')
 
 var target = ['cricket'];
  var api = new Twit({
-    consumer_key:         'HtP114me0TaOFYFEDDwMYA'
-  , consumer_secret:      'jyW1VCEDsAC58zXZv5m0sz8JBhN23h6QmIgMhFv0EY'
-  , app_only_auth: true
-  , access_token:         '179249265-9tO98UWDoP6NmuSdeW03aYO0dRijtGm6X0sQ9MZ5'
-  , access_token_secret:  'HAoyGuGYdInkKQ017JIryx8UloS9ncoCZTDeM2eZCI'
+    consumer_key:         'okfPHS3Hgdedz0S0bvutrmDNl'
+  , consumer_secret:      '7CYNEAA2WyDlHIW3Hx8M51FUV8ZMvECuTu5X6nhaUB45oyjQJq'
+  , access_token:         '179249265-LKqFINxf4f7R3Mubde7jBAYYsdP8HCUH0E789Owj'
+  , access_token_secret:  'inF9vTELjKqDvCppBPvRWP7PsWte8Z0RNDae5rcie1ObO'
 })
 
  
